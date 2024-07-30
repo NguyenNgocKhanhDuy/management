2fb6a704480cd1a8675396908fbe70616cd085b1
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-07-30 14:37:16 +0700

    feat(Management UI): add sidebar ui

--- src/component/Management.tsx
+++ src/component/Management.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import "../assets/style/base.scss";
+import "../assets/style/management.scss";
 import blackImg from "../assets/img/black.jpg";
 
 function Management() {
 	return (
-		<div>
+		<div className="container">
 			<div className="sidebar">
-				<h2>obantor</h2>
+				<h2 className="title">obantor</h2>
 				<div className="item">
 					<i className="fa-solid fa-list-check"></i>
 					<span>Task</span>
+					<span className="amount">12</span>
 				</div>
 				<div className="item">
 					<i className="fa-solid fa-diagram-project"></i>
@@ -20,14 +21,12 @@ function Management() {
 					<span>Note</span>
 				</div>
 				<div className="account">
-                    <img src={blackImg} alt="avatar" className="avatar" />
-					<div className="info">
-						<span className="name">Duy Nguyen</span>
-						<span className="email">duynguyenngockhanh@gmail.com</span>
-					</div>
+					<img src={blackImg} alt="avatar" className="avatar" />
+					<span className="name">Duy Nguyen</span>
+					<i className="fa-solid fa-angle-right"></i>
 				</div>
 			</div>
-			<div className="main-container"></div>
+			<div className="main-container">Right</div>
 		</div>
 	);
 }

5cd9913da6bfeb065efc272ef3b56e67d5b000a2
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-07-30 16:30:52 +0700

    feat(Management Task Ui): add top(search, notice)

--- src/component/Management.tsx
+++ src/component/Management.tsx
@@ -7,7 +7,7 @@ function Management() {
 		<div className="container">
 			<div className="sidebar">
 				<h2 className="title">obantor</h2>
-				<div className="item">
+				<div className="item isItemSelected">
 					<i className="fa-solid fa-list-check"></i>
 					<span>Task</span>
 					<span className="amount">12</span>
@@ -26,7 +26,24 @@ function Management() {
 					<i className="fa-solid fa-angle-right"></i>
 				</div>
 			</div>
-			<div className="main-container">Right</div>
+			<div className="main-container">
+				<div className="top">
+					<div className="greeting">
+						Welcome,
+						<br />
+						<span className="name">Duy Nguyen</span>
+					</div>
+					<div className="search">
+						<i className="fa-solid fa-magnifying-glass"></i>
+						<input type="text" className="searchInput" placeholder="Search here..." />
+					</div>
+					<div className="notice">
+						<i className="fa-regular fa-bell"></i>
+						<i className="fa-regular fa-circle-question"></i>
+					</div>
+				</div>
+				<div className="content">Content</div>
+			</div>
 		</div>
 	);
 }

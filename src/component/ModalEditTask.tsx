ad581664ff317434312400d5488610fc52bc6dca
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-08-02 19:38:23 +0700

    feat(Management task UI): fix date, show hide edit tag (modal edit task)

--- src/component/ModalEditTask.tsx
+++ src/component/ModalEditTask.tsx
@@ -1,27 +1,36 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../assets/style/modalEditTask.scss";
 import blackImg from "../assets/img/black.jpg";
+import DatePicker  from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from "date-fns";
 
 function ModalEditTask() {
-
-    const inputCategoryRef = useRef<HTMLInputElement>(null);
+	const inputCategoryRef = useRef<HTMLInputElement>(null);
 	const categoryTestRef = useRef<HTMLDivElement>(null);
-    const selectColorRef = useRef<HTMLSelectElement>(null);
-    
+	const selectColorRef = useRef<HTMLSelectElement>(null);
+	const [isEditCategory, setIsEditCategory] = useState(false);
+	const [deadline, setDeadline] = useState<Date | null>(new Date());
 
-    const handleChangeCategoryTest = () => {
-			if (inputCategoryRef.current && categoryTestRef.current) {
-				var text = inputCategoryRef.current.value;
-				categoryTestRef.current.innerText = text.length > 0 ? text : "Category...";
-			}
-		};
+	const handleChangeCategoryTest = () => {
+		if (inputCategoryRef.current && categoryTestRef.current) {
+			var text = inputCategoryRef.current.value;
+			categoryTestRef.current.innerText = text.length > 0 ? text : "Category...";
+		}
+	};
 
-		const handleSelectColor = (event: any) => {
-            if (selectColorRef.current && categoryTestRef.current) {
-				selectColorRef.current.style.backgroundColor = event.target.value;
-				categoryTestRef.current.style.backgroundColor = event.target.value;
-			}
-		};
+	const handleSelectColor = (event: any) => {
+		if (selectColorRef.current && categoryTestRef.current) {
+			selectColorRef.current.style.backgroundColor = event.target.value;
+			categoryTestRef.current.style.backgroundColor = event.target.value;
+		}
+	};
+
+	const handleChangeDateFormat = (dateString : string) => {
+		const date = new Date(dateString);
+		const formatDate = format(date, 'MMMM do, yyyy');
+		return formatDate;
+	}
 
 	return (
 		<div>
@@ -34,35 +43,45 @@ function ModalEditTask() {
 							<img src={blackImg} alt="avatar-creator" className="avatar" />
 							<p className="name">Nguyen Ngoc Khanh Duy</p>
 						</div>
-						<div className="date">
-							<div className="date-create">
-								<i className="fa-regular fa-calendar"> :</i>
-								2024/4/6
-							</div>
-							<div className="date-deadline">
-								<i className="fa-regular fa-clock"> :</i>
-								2024/4/6
-							</div>
+						<div className="date-create">
+							<i className="fa-regular fa-calendar"> :</i>
+							<span>{handleChangeDateFormat("2024-08-02")}</span>
 						</div>
 					</div>
 					<div className="category">
-						<div className="category-text" ref={categoryTestRef}>Category...</div>
-						<button className="category-btn">Chỉnh sửa nhãn</button>
+						<div className="category-text" ref={categoryTestRef}>
+							Category...
+						</div>
+						<button
+							className="category-btn"
+							onClick={() => {
+								setIsEditCategory(!isEditCategory);
+							}}
+						>
+							{isEditCategory ? "Save Change" : "Edit Tag"}
+						</button>
 					</div>
 
-					<div className="category-edit">
-						<input type="text" className="category-edit-name" placeholder="Category..." ref={inputCategoryRef} onInput={handleChangeCategoryTest} />
-						<select className="category-edit-color" onChange={handleSelectColor} ref={selectColorRef}>
-							<option className="category-edit-color-item" value="cornflowerblue"></option>
-							<option className="category-edit-color-item" value="plum"></option>
-							<option className="category-edit-color-item" value="orange"></option>
-							<option className="category-edit-color-item" value="pink"></option>
-						</select>
-					</div>
-
+					{isEditCategory ? (
+						<div className="category-edit">
+							<input type="text" className="category-edit-name" placeholder="Category..." ref={inputCategoryRef} onInput={handleChangeCategoryTest} />
+							<select className="category-edit-color" onChange={handleSelectColor} ref={selectColorRef}>
+								<option className="category-edit-color-item" value="cornflowerblue"></option>
+								<option className="category-edit-color-item" value="plum"></option>
+								<option className="category-edit-color-item" value="orange"></option>
+								<option className="category-edit-color-item" value="pink"></option>
+							</select>
+						</div>
+					) : (
+						""
+					)}
 					<div className="desc">
 						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores numquam quibusdam dolor possimus quam et doloremque dolorum reiciendis nam tempora temporibus eveniet eaque ex natus sed repudiandae inventore, quaerat dolores. Lorem ipsum, dolor sit amet consectetur adipisicing
 						elit. Dolores facilis, aspernatur quae architecto laudantium repellendus corporis perferendis quam quasi ipsum unde laborum qui libero vero quaerat quis cumque beatae saepe!
+					</div>
+					<div className="date-deadline">
+						<i className="fa-regular fa-clock"> :</i>
+						<DatePicker selected={deadline} onChange={(date) => setDeadline(date)} showTimeSelect dateFormat="MMMM do YYYY, hh:mm:ss a" className="date-time" showYearDropdown yearDropdownItemNumber={100} scrollableYearDropdown />
 					</div>
 				</div>
 			</div>

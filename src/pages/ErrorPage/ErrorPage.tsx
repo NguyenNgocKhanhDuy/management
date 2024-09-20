d06056766d0f7b74955d9151fc32012f863be32e
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-09-20 19:26:00 +0700

    feat: error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./errorPage.scss";
 
 function ErrorPage() {
-	return <div>Error</div>;
+	return (
+		<div className="error">
+			<div className="error__wrap">
+				<div className="error__title">Error</div>
+				<div className="error__subtitle">404 Page Not Found</div>
+				<div className="error__description">The page you're looking for might have been moved or deleted.</div>
+				<Link to={"/login"} className="error__link">
+					Go to Login
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default ErrorPage;

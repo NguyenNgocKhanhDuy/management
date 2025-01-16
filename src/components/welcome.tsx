f24af05ed0646709a7f6a9c89bf5e1c1c40a5857
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-01-16 17:12:59 +0700

    fix: redirect at welcome

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -3,8 +3,15 @@ import { useLocation, Navigate } from "react-router-dom";
 
 const Welcome = () => {
 	const location = useLocation();
+	const queryParams = new URLSearchParams(location.search);
+	const invite = queryParams.get("invite");
 
-	return <Navigate to={`/login${location.search}`} />;
+	if (invite == "yes") {
+		const token = queryParams.get("token");
+		return <Navigate to={`/confirm?token=${token}`} />;
+	} else {
+		<Navigate to={`/login`} />;
+	}
 };
 
 export default Welcome;
